844396a027a62c27ad5fa1804d374a17c7b41285
tcanbolat <kangalcanbolat@gmail.com>
2023-07-19 12:31:04 -0700

    Installed Expo & React Native

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, SafeAreaView, Image } from 'react-native'
+import Logo from "./assets/splash.png"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <SafeAreaView style={styles.container}>
+        <StatusBar/>
+        <Text>Hello world</Text>
+        <Image style={styles.image} source={Logo} resizeMode="contain" />
+      </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
-});
+  image: {
+    width: '25%',
+    height: '25%'
+  }
+})
