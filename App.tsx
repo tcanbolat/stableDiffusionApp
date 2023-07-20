0f2d7d2e88691022a2efc0db2b80ab023343e3a2
tcanbolat <kangalcanbolat@gmail.com>
2023-07-19 17:44:32 -0700

    installed gluestack

--- App.tsx
+++ App.tsx
@@ -1,14 +1,21 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View, Image } from 'react-native'
+
+import { config } from "./gluestack-style.config"
+import { StyledProvider } from "@gluestack-style/react"
+
 import Logo from './assets/icon.png'
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app</Text>
-      <Image style={styles.image} source={Logo} />
-      <StatusBar style="auto" />
-    </View>
+    <StyledProvider config={config}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app</Text>
+        <Image style={styles.image} source={Logo} />
+        <StatusBar style="auto" />
+      </View>
+    </StyledProvider>
   )
 }
 
