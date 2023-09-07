4224609e01a61f3e3ff7a49a2f4042c04bee2577
tcanbolat <kangalcanbolat@gmail.com>
2023-09-07 11:26:00 -0700

    update

--- api/src/server.py
+++ api/src/server.py
@@ -58,4 +58,5 @@ if __name__ == "__main__":
         host="0.0.0.0",
         port=int(os.getenv("PORT", 8000)),
         auto_reload=os.getenv("SANIC_AUTORELOAD", False),
+        debug=os.getenv("SANIC_DEBUG", False),
     )
--- client/babel.config.js
+++ client/babel.config.js
@@ -2,6 +2,7 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['@gluestack-style/babel-plugin-styled-resolver', 'expo-router/babel'],
+    plugins: ['expo-router/babel'],
   };
 };
+// '@gluestack-style/babel-plugin-styled-resolver',
